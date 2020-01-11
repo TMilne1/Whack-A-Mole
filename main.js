37ced8de75b1ee23ef9a909ba60f264d29f8a68a
tmilne1 <tmilne1@gmail.com>
2020-01-11 11:24:06 -0500

    put components together, keeps score as moles are clicked

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
 let last;
-
 let score=0;
+let timeUp=false;
 
 function randomTime(min,max){
     return Math.round(Math.random()*(max-min) + min)  
@@ -20,25 +20,39 @@ function randomHole(holes){
     return current
 }
 
-function play(){
+function play(timer = 10000){
     score = 0;
-
+    scoreBoard.textContent = score;
+    timeUp = false;
+    peek();
+    setTimeout(()=>{
+        timeUp = true;
+    },timer) 
+     
 }
 
 function peek(){
-    const time = randomTime(2000,10000)
+    const time = randomTime(300,1000)
     const hole = randomHole(holes)
     //return hole;
     hole.classList.add('up')
-
-    setInterval(()=>{
+    setTimeout(()=>{
         hole.classList.remove('up')
-        
+        if (!timeUp) { peek() }
     },time)
+    
+}
+
+function scoreKeeper(){
+    if (!event.isTrusted) {return 'cheater';}
+    this.classList.remove('up')  
+    score += 1
+    scoreBoard.textContent = score;
+    
 }
 
 moles.forEach((mole)=>{
-    mole.addEventListener('click',()=>score+=1)
+    mole.addEventListener('click',scoreKeeper)
 })
 
 
